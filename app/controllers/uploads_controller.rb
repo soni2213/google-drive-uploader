6c09802327fdd4e3f81b4e36b5264c5d07ce88a4
Soni <soni@cybrilla.com>
2020-05-03 22:50:40 +0530

    Fix lint issues

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
+# To handle file uploads
 class UploadsController < ApplicationController
+  ALLOWED_FILE_TYPES = ['.png'].freeze
+
   before_action :create_collection, only: [:upload]
-  ALLOWED_FILE_TYPES = [".png"]
 
   def upload
     if ActiveModel::Type::Boolean.new.cast(file_params[:enable_filter])
@@ -8,9 +12,8 @@ class UploadsController < ApplicationController
     else
       upload_file
     end
-
-    rescue StandardError => e
-      render json: "Error occured, #{e}", status: Unprocessable_entity
+  rescue StandardError => e
+    render json: "Error occured, #{e}", status: :unprocessable_entity
   end
 
   private
@@ -21,26 +24,32 @@ class UploadsController < ApplicationController
   end
 
   def create_collection
-    session.root_collection.create_subcollection("riskcovry_uploads") if !session.collection_by_title("riskcovry_uploads")
+    return if collection
+
+    google_session.root_collection.create_subcollection('riskcovry_uploads')
   end
 
   def upload_file
-    collection = session.collection_by_title("riskcovry_uploads")
-    file = session.upload_from_file(file_params[:file].tempfile, file_params[:file].original_filename, convert: false)
+    file = google_session.upload_from_file(file_params[:file].tempfile,
+                                           file_params[:file].original_filename)
     collection.add(file)
-    session.root_collection.remove(file)
-    render json: "Upload Successful"
+    google_session.root_collection.remove(file)
+    render json: 'Upload Successful'
   end
 
   def upload_filtered_file
     if ALLOWED_FILE_TYPES.include? File.extname(file_params[:file])
       upload_file
     else
-      render json: "Only .png extension file is allowed to upload", status: 422
+      render json: 'Only .png extension file is allowed to upload', status: 422
     end
   end
 
-  def session
-    @session ||= GoogleDrive::Session.from_config("config/google_drive.json")
+  def google_session
+    @google_session ||= GoogleDrive::Session.from_config('config/google_drive.json')
+  end
+
+  def collection
+    @collection ||= google_session.collection_by_title('riskcovry_uploads')
   end
 end
